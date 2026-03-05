3b6b26de6909e95f9bbcb0601dde67ebcfbe8e9f
Peter Conti <petertconti@gmail.com>
2026-03-05 12:49:36 -0500

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'payload-plugin-gmc-ecommerce/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'payload-plugin-gmc-ecommerce/rsc'
+
+export const importMap = {
+  'payload-plugin-gmc-ecommerce/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'payload-plugin-gmc-ecommerce/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
